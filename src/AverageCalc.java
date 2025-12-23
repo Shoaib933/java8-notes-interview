8d9eb9cc3820a1ea31a1d98a51280199adf3f853
shoaib <shasanex@gmail.com>
2025-12-23 23:39:44 +0530

    git commmit -m "New way to CALC AVERAGE"

--- src/AverageCalc.java
+++ src/AverageCalc.java
@@ -10,5 +10,14 @@ public class AverageCalc {
                 .average().orElse(0.0);
 
         System.out.println("Avg Number : "+avg);
+        /*
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40);
+
+double average = numbers.stream()
+        .collect(Collectors.averagingInt(Integer::intValue));
+
+System.out.println(average); // 25.0
+
+         */
     }
 }
