9a7f3af12f5071d0ded098f83371329d8b75e6d3
shoaib <shasanex@gmail.com>
2026-01-11 23:03:02 +0530

    git commmit -m "code optimized"

--- src/GroupStringByLength.java
+++ src/GroupStringByLength.java
@@ -1,15 +1,44 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class GroupStringByLength {
 
     static  void groupStrByLength() {
-        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+        IO.println("Grouping By Length");
+        List<String> names = Arrays.asList("Alice", "Three", "Shoaib","Bob",
+                "Charlie", "Seven", "Six", "Eight");
 
         Map<Integer, List<String>> grp = names.stream()
                 .collect(Collectors.groupingBy(String::length));
         System.out.println(grp);
+
+        IO.println("Sorting In reverse order");
+
+        List<Map.Entry<Integer, List<String>>> rev  = grp.entrySet().stream()
+                .sorted(Map.Entry.<Integer, List<String>>comparingByKey().reversed())
+                .toList();
+
+        IO.println(rev);
+
+//        ✅ If You Want Only the VALUES (List<String>)
+                List<List<String>> revValList =
+                grp.entrySet().stream()
+                        .sorted(Map.Entry.<Integer, List<String>>comparingByKey().reversed())
+                        .map(Map.Entry::getValue)
+                        .collect(Collectors.toList());
+        IO.println(revValList);
+            // Very Confusion for me
+//        ✅ If You Want a Sorted Map (Descending Key Order) ⭐ (Best)
+                Map<Integer, List<String>> sortedMap =
+                grp.entrySet().stream()
+                        .sorted(Map.Entry.<Integer, List<String>>comparingByKey().reversed())
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (a, b) -> b,
+                                LinkedHashMap::new
+                        ));
+        IO.println("If You Want a Sorted Map (Descending Key Order) ⭐ (Best)");
+        IO.println(sortedMap);
     }
 }
--- src/MaxValue.java
+++ src/MaxValue.java
@@ -1,11 +1,22 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 
 public class MaxValue {
     static void maxVal() {
         List<Integer> numbers = Arrays.asList(10, 20, 30, 40);
 //        int n = numbers.stream().max(Integer::compare).orElse(0);
         int n = numbers.stream().max(Integer::compare).get();
+        /*
+        IO.println("Max Numebr IMPL : "+
+                numbers.stream().max(Comparator.comparingInt(a -> {
+                    IO.println("Hi : "+a *2);
+                    return a * 2;
+
+                })).get());
+
+         */
         System.out.println("Maxx :" +n);
     }
 }
