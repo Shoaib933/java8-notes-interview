0804fec28725a7c1b33ab6e87442924919568141
shoaib <shasanex@gmail.com>
2026-01-24 01:24:48 +0530

    git commmit -m " First Repeated character"

---
+++ src/interview/FirstRepeatedCharLT2351.java
@@ -0,0 +1,39 @@
+package interview;
+// First Letter to Appear Twice - Lt-2351
+public class FirstRepeatedCharLT2351 {
+    static void main() {
+        String s = "abcdd";
+        char ch = firstRepeatedChar(s);
+
+        IO.println("First5 Repeated char : "+ch);
+        /*
+        // to understand concept
+        String s = "abca";
+    int[] count = new int[26];
+
+    for (char ch : s.toCharArray()) {
+        IO.println(" : "+count[ch - 'a']++);
+
+    }
+
+// Print frequencies
+    for (int i = 0; i < 26; i++) {
+        if (count[i] > 0) {
+            System.out.println((char)(i + 'a') + " : " + count[i]);
+        }
+    }
+         */
+
+    }
+    static char firstRepeatedChar(String s) {
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            if(count[ch - 'a'] > 0)
+                return ch;
+            else
+                count[ch - 'a']++;
+        }
+        return '\0';
+    }
+}
