152644bde6bfa92cf61b9bdf682e4a5010c42785
shoaib <shasanex@gmail.com>
2026-01-23 23:29:31 +0530

    git commmit -m " Remove duplicate frome sorted arra"

---
+++ src/interview/RemDupSortArrayLT80.java
@@ -0,0 +1,30 @@
+package interview;
+/*
+Condition --- sorted array
+Result --- remove some duplicates in-place such that each unique element appears at most twice.
+ */
+public class RemDupSortArrayLT80 {
+    static void main() {
+        int[] arr = {1,1,1,2,2,3};
+
+        int newLiength = removeDup(arr);
+
+        for (int i = 0; i < newLiength; i++) {
+            IO.print( arr[i]+" ");
+        }
+    }
+
+    static int removeDup(int[] arr) {
+        if (arr.length<=2)
+            return arr.length;
+        int slow = 2;
+        for (int fast = 0; fast < arr.length; fast++) {
+
+            if (arr[slow-2] != arr[fast]) {
+                arr[slow] = arr[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
