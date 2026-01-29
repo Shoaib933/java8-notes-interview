75dfb76617ea38af6ffcb6124c37df9c4c43c77f
shoaib <shasanex@gmail.com>
2026-01-30 00:11:30 +0530

    git commmit -m " remove Elemnt"

---
+++ src/interview/removeSpecElemArray.java
@@ -0,0 +1,25 @@
+package interview;
+
+public class removeSpecElemArray {
+
+    static void main() {
+
+        int[] arr = {3, 2, 2, 3};
+//        int[] arr = {0, 1, 2, 2, 3, 0, 4, 2};
+        int rmvElem = 2;
+        int p=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != rmvElem) {
+                arr[p] = arr[i];
+                p++;
+            }
+        }
+
+        for (int i = 0; i < p; i++) {
+            IO.println(arr[p]);
+        }
+
+
+    }
+}
