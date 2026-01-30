edcae34e6e0c5dab277500f0ca7ecd352797b1b2
shoaib <shasanex@gmail.com>
2026-01-31 00:35:49 +0530

    git commmit -m " remove Elemnt"

--- src/CountFrequency.java
+++ src/CountFrequency.java
@@ -1,5 +1,6 @@
 import java.util.Locale;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CountFrequency {
@@ -8,7 +9,8 @@ public class CountFrequency {
         String input = "java";
         Map<Character, Long> freq = input.chars()
                 .mapToObj(ch -> (char)ch)
-                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+//                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         System.out.println("Frequency : "+freq);
     }
 }
